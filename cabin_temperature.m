5fc242aecdf8f1bb50d7d134d16e45b7ce3815d0
nixiaohelalala <160839305+nixiaohelalala@users.noreply.github.com>
2024-05-30 01:12:51 +0800

    Add files via upload

---
+++ cabin_temperature.m
@@ -0,0 +1,21 @@
+Table 1 - Output to screen formatting example
+Data logging initiated - 29/05/2024 23:10:40
+Location - Nottingham
+
+Minute	Temperature (°C)
+0.00	-38.27
+1.00	-50.00
+2.00	-50.00
+3.00	-50.00
+4.00	-50.00
+5.00	-50.00
+6.00	-50.00
+7.00	-50.00
+8.00	-50.00
+9.00	-50.00
+
+Max temp	-38.27 °C
+Min temp	-50.00 °C
+Average temp	-49.98 °C
+
+Data logging initiated - 29/05/2024 23:10:40
